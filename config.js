20a5b8fb232d19708792514d69c947c0c4d9c61e
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-09-20 15:05:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@
 //Dalam file _menunya.js
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx']  // isi jika perlu
-global.mods = ['6282xxxxxx']   // isi jika perlu
-global.prems = ['6282xxxxxx']  // isi jika perlu
+global.owner = ['6287793236775']  // isi jika perlu
+global.mods = ['6287793236775']   // isi jika perlu
+global.prems = ['6287793236775']  // isi jika perlu
 
 //Isi dengan nama dan nomor lu//
-global.nameowner = '⫹⫺ OWNER' // isi nama kalian bebas
-global.numberowner = '6282xxxxxx' // isi nomor kalian dengan awalan seperti di contoh 
-global.dana = '6282xxxxxx' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '6282xxxxxx' // isi dengan nomor pulsa hp kalian
-global.gopay = '6282xxxxxx'
+global.nameowner = '⫹⫺ Aji Bekasi' // isi nama kalian bebas
+global.numberowner = '6287793236775' // isi nomor kalian dengan awalan seperti di contoh 
+global.dana = '6287793236775' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '6287793236775' // isi dengan nomor pulsa hp kalian
+global.gopay = '6282147562437'
 
 //Name Bot & Grouplink//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'SxyoBotz-MD'
+global.gc = 'https://chat.whatsapp.com/JS11vcFkCpM93SycO5Z9tI'
+global.web = 'https://www.instagram.com/ahmdzakaria47' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 
 //Tanpilan wm Bot //
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© SxyoBotz-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ SxyoBotz'
+global.wm3 = '© SxyoBotz-MD'
 global.wm4 = '© 2021/2022'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -35,8 +35,8 @@ global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.packname = 'SxyoBot-MD'
+global.author = '@Aji\nRama\nKontol'
 
 //Apikey Setting//
 global.APIs = { // API Prefix
